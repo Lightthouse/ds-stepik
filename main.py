1f05ec3a3dfb0a28ab5342f27d27104ccfed6700
Stepan <darkpolarbear42@gmail.com>
2023-03-03 22:07:40 +0500

    Прошел курс "Основы анализа python"

--- main.py
+++ main.py
@@ -2,3 +2,20 @@ import numpy as np
 import pandas as pd
 from pprint import pprint
 
+num = int(input())
+
+
+def fib(n):
+    res = [0, 1]
+    if n < 3:
+        return res[0:n]
+
+    counter = 2
+    while counter < n:
+        numb = res[-1] + res[-2]
+        res.append(numb)
+        counter += 1
+
+    print(*res)
+
+fib(num)
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 
 [Демоверсия курса «Аналитик данных» школы karpov.courses](https://stepik.org/course/74457)
 
+ [Основы анализа python](https://stepik.org/course/116618/syllabus)
