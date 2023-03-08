4dccf139d0f9268175c4cb61a05448ecfa9b0b0c
Stepan <darkpolarbear42@gmail.com>
2023-03-08 14:06:22 +0500

    Прошел курс "Основы статистики"

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import numpy as np
 import pandas as pd
 from pprint import pprint
+import random
 
-num = int(input())
+# print(random.seed(10))
+
+import scipy.stats as st
+import math
 
 
-def fib(n):
-    res = [0, 1]
-    if n < 3:
-        return res[0:n]
+x_men_bar = 45
+sd_men = 9
 
-    counter = 2
-    while counter < n:
-        numb = res[-1] + res[-2]
-        res.append(numb)
-        counter += 1
+x_w_bar = 34
+sd_w = 10
 
-    print(*res)
+n = 100
+alpha = 0.95
 
-fib(num)
+se = math.sqrt(sd_men**2 / n + sd_w**2 / n)
+t = (x_men_bar - x_w_bar) / se
+
+df = n + n - 2
+
+p_value = np.abs(st.t.ppf((1 - alpha) / 2, df))
+
+print(f"""t-value: {t}
+p-value for the {alpha * 100}% confidence interval: {p_value}
+""")
+if t > p_value:
+    print("rejecting the null hypothesis")
+elif t < p_value:
+    print("acepting the null hypothesis")
+
+    from scipy import stats
+
+    ...
+
+from scipy import stats
+...
+print(stats.f.sf(3.5, 3, 15))
